12b077d8266756917e3694c605a24098cc010ca9
Amlaith <47604982+Amlaith@users.noreply.github.com>
2023-11-04 00:44:43 +0300

    Added audio recording

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect
 from render_answer import render_answer
 from classifier import decode_command
+import os
+import uuid
 
 
+UPLOAD_FOLDER = '..\\data\\audio'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # d = {'one': 'that\'s one', 'two': 'this is two', 'three': 'three here'}
 # @app.route("/")
@@ -28,13 +32,22 @@ def index():
     # Render the template with the corresponding text
     return render_template('index.html', answer=answer)
 
-# Create a template for the form
-# @app.template_filter('render_answer')
-# def render_answer(answer):
-#     if answer is not None:
-#         return answer
-#     else:
-#         return ''
+@app.route('/save-record', methods=['POST'])
+def save_record():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    file_name = str(uuid.uuid4()) + ".mp3"
+    full_file_name = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+    file.save(full_file_name)
+    return '<h1>Success</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)    
